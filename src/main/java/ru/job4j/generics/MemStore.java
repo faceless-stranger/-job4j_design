cdb77e81759b7d147748ed08f9df00c962be6873
PC <faceless-stranger@mail.ru>
2023-06-25 15:48:00 +0300

    1. Что такое обобщенные типы (generics) [#4952  [#4952]]

--- src/main/java/ru/job4j/generics/MemStore.java
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -16,11 +16,7 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean replace(String id, T model) {
-        if (storage.containsKey(id)) {
-            storage.replace(id, model);
-            return true;
-        }
-        return false;
+        return storage.containsKey(id) ? (storage.replace(id, model) != null) : false;
     }
 
     @Override
