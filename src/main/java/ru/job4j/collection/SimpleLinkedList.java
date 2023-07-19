084e5a3288be07f5f0e204398418755dcd5d896d
PC <faceless-stranger@mail.ru>
2023-07-19 14:40:10 +0300

    1. Динамический список на массиве. [#158  [#158]]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,73 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+
+    @Override
+    public void add(E value) {
+        Node newNode = new Node<>(value, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node item = head;
+            while (item.next != null) {
+                item = item.next;
+            }
+            item.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node value = head;
+        for (int i = 0; i < index; i++) {
+            value = value.next;
+        }
+        return (E) value.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int expectedModCount = modCount;
+            Node iteratorIndex = head;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException("hasNext колекция была изменена");
+                }
+                return iteratorIndex != null;
+            }
+
+            @Override
+            public E next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException("hasNext колекция была изменена");
+                }
+                E value = (E) iteratorIndex.item;
+                iteratorIndex = iteratorIndex.next;
+                return (E) value;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
