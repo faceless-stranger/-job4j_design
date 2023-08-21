e1e569139b673b06d065b5a0267eae09fc2d937c
PC <faceless-stranger@mail.ru>
2023-08-21 23:46:19 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,46 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        Optional<Node<E>> nodeParent = findBy(parent);
+        if (nodeParent.isPresent()) {
+            nodeParent.get().children.add(new Node<>(child));
+        }
+        findBy(parent);
+        return rsl;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        /** Он создает пустой объект типа Optional, который не содержит значения. */
+        Optional<Node<E>> rsl = Optional.empty();
+         /** создает очередь с именем data, которая может содержать элементы типа Node<E> */
+        Queue<Node<E>> data = new LinkedList<>();
+         /**Пытается вставить элемент в конец очереди и возвращает true, если вставка прошла успешно, и false, если вставка не удалась */
+        data.offer(this.root);
+         /**Проверяет что data не пустая */
+        while (!data.isEmpty()) {
+         /** удаляет элемент из начала очереди и присвавает el/ если очеред пуста вернет нулл */
+            Node<E> el = data.poll();
+            /** Проверяет равенл ли элемент нашему значению */
+            if (el.value.equals(value)) {
+         /** присваеиваем рсл хначение el и если оно null бросает exeption */
+                rsl = Optional.of(el);
+                break;
+            }
+         /** ?? */
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}
