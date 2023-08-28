c56c99959ebdc89a1ad91570666efff64e755ea3
PC <faceless-stranger@mail.ru>
2023-08-28 19:21:26 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -9,41 +10,39 @@ public class SimpleTree<E> implements Tree<E> {
         this.root = new Node<>(root);
     }
 
+    public boolean isBinary() {
+        return findByPredicate(eNode -> eNode.children.size() > 2).isEmpty();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (condition.test(el)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+
     @Override
     public boolean add(E parent, E child) {
-        boolean rsl = false;
         Optional<Node<E>> nodeParent = findBy(parent);
         Optional<Node<E>> nodeChild = findBy(child);
-        if (nodeParent.isPresent()
-                && nodeParent.get().value == parent
-                && nodeChild.isEmpty()) {
+        boolean rsl = nodeParent.isPresent() && nodeChild.isEmpty();
+        if (rsl) {
             nodeParent.get().children.add(new Node<>(child));
-            rsl = true;
         }
         return rsl;
     }
 
     @Override
     public Optional<Node<E>> findBy(E value) {
-        /** Он создает пустой объект типа Optional, который не содержит значения. */
-        Optional<Node<E>> rsl = Optional.empty();
-         /** создает очередь с именем data, которая может содержать элементы типа Node<E> */
-        Queue<Node<E>> data = new LinkedList<>();
-         /**Пытается вставить элемент в конец очереди и возвращает true, если вставка прошла успешно, и false, если вставка не удалась */
-        data.offer(this.root);
-         /**Проверяет что data не пустая */
-        while (!data.isEmpty()) {
-         /** удаляет элемент из начала очереди и присвавает el/ если очеред пуста вернет нулл */
-            Node<E> el = data.poll();
-            /** Проверяет равенл ли элемент нашему значению */
-            if (el.value.equals(value)) {
-         /** присваеиваем рсл значение el и если оно null бросает exeption */
-                rsl = Optional.of(el);
-                break;
-            }
-         /** ?? */
-            data.addAll(el.children);
-        }
-        return rsl;
+        return findByPredicate(e -> e.value.equals(value));
+
     }
 }
