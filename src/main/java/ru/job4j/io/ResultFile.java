ed2c52fa9c299d0b509056deb1a50016d584f015
PC <faceless-stranger@mail.ru>
2023-09-27 13:50:26 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/resultMultiple.txt")) {
+            out.write("1*1=1".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*2=2".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*3=3".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*4=4".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*5=5".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*6=6".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*7=7".getBytes());
+            out.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
