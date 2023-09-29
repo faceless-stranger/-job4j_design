3c8969e9783a9ecd6d36309892daf6178b49776d
PC <faceless-stranger@mail.ru>
2023-09-29 17:45:14 +0300

    2.0. FileOutputStream [#252488 #448174]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,18 +1,19 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
-        try {
-            FileInputStream in = new FileInputStream("data/even.txt.");
-            int read;
-            boolean rsl;
-            while ((read = in.read()) != -1) {
-                rsl = read % 2 == 0;
-                System.out.println(rsl);
+        try (FileInputStream in = new FileInputStream("data/even.txt")) {
+            StringBuilder even = new StringBuilder();
+            int number;
+            while ((number = in.read()) != -1) {
+                even.append((char) number);
+                // rsl = number % 2 == 0;
+                System.out.println(even);
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
