ff7362465eaf550e3e1a4b9813300c663d6eb517
PC <faceless-stranger@mail.ru>
2023-10-22 22:24:08 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Vlad");
+    }
+
+    @Test
+    void whenInvalidFormatComments() {
+        String path = "./data/pair_without_comment_Invalid.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Vlad");
+    }
+
+    @Test
+    void whenInvalidFormatKeyAndValue() {
+        String path = "./data/pair_without_comment_Invalid_Key_Value.properties";
+        Config config = new Config(path);
+        assertThrows(IllegalArgumentException.class, () -> config.load());
+    }
+
+    @Test
+    void whenDoubleIsEqual() {
+        String path = "./data/pair_without_comment_double_equals.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("=Vlad");
+    }
+}
