d3f6c8de6476af08fbb82c66f7b9e261a80ae293
PC <faceless-stranger@mail.ru>
2023-10-27 16:07:26 +0300

    8. Тестирование IO [#173905 #455165] fix

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.io;
 
 import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+
 import java.io.*;
 import java.nio.file.Path;
 
@@ -10,18 +12,19 @@ class AnalysisTest {
 
     @Test
     void drop(@TempDir Path tempDir) throws IOException {
-        File server = new File("server.txt");
-        File target = new File("target.txt");
+        File server = tempDir.resolve("server.txt").toFile();
         try (PrintWriter out = new PrintWriter(server)) {
-            out.print("200 10:56:01\n" +
-                    "300 10:57:01\n" +
-                    "400 10:58:01\n" +
-                    "300 10:59:01\n" +
-                    "500 11:01:02\n" +
-                    "200 11:02:02");
+            out.print("200 10:56:01\n"
+                    + "300 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "300 10:59:01\n"
+                    + "500 11:01:02\n"
+                    + "200 11:02:02");
+            System.out.println(out);
         }
         Analysis analysis = new Analysis();
-        analysis.unavailable("server.txt", "target.txt");
+        File target = tempDir.resolve("target.csv").toFile();
+        analysis.unavailable(server.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(rsl::append);
