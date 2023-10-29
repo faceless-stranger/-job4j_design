6962dedffd496713acbb22a3934177119ddb20e1
PC <faceless-stranger@mail.ru>
2023-10-29 13:56:10 +0300

    10. Сканирование файловой системы [#106929] ???

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles implements FileVisitor<Path> {
+    private Predicate<Path> condition;
+    private ArrayList<Path> result = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (condition.test(file)) {
+            result.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return result;
+    }
+}
