f23ca3e4cc18f9b57cd58b7afb57a3fa2c3ff86d
Владислав <faceless-stranger@mail.ru>
2023-11-06 05:53:22 +0300

    13. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,68 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        /* TODO добавить необходимые проверки. */
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException(String.format("This key: '%s' is missing", key));
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        /* TODO разобрать аргументы на значения. */
+        for (String arg : args) {
+            String[] st = arg.split("=", 2);
+            values.put(st[0].substring(1), st[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        /* TODO добавьте необходимые проверки. */
+        validate(args);
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void validate(String[] args) {
+        /* Проверка что  массив не пустой*/
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+
+        for (String arg : args) {
+            /* Проверка на наличие разделителя */
+            if (!arg.contains("=")) {
+                throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain an equal sign", arg));
+            }
+            /* Проверка на наличие - */
+            if (!arg.startsWith("-")) {
+                throw new IllegalArgumentException(String.format("Error: This argument '%s' does not start with a '-' character", arg));
+            }
+            /* Проверка на наличия key/value */
+            String[] st = arg.split("=", 2);
+            String key = st[0].substring(1);
+            String value = st[1];
+            if (key.isEmpty()) {
+                throw new IllegalArgumentException(String.format("Error: This argument '%s=%s' does not contain a key", st[0], st[1]));
+            } else if (value.isEmpty()) {
+                throw new IllegalArgumentException(String.format("Error: This argument '%s=%s' does not contain a value", st[0], st[1]));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
