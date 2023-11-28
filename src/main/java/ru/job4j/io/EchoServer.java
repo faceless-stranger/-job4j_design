03b47e46872ca02a16528bd9b1d8c7719ce890d7
PC <faceless-stranger@mail.ru>
2023-11-28 19:00:40 +0300

    3. Бот [#7921 #462946] fix 3

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -8,7 +8,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    private  static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
 
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -20,14 +20,14 @@ public class EchoServer {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     out.write("Hello, dear friend.\r\n\r\n".getBytes());
                     String value = in.readLine().split("\\?")[1].split(" ")[0];
-                    if (value.equals("msg=Exit")) {
+                    if ("msg=Exit".equals(value)) {
                         out.write("End job".getBytes());
                         break;
                     }
-                    if (value.equals("msg=Hello")) {
+                    if ("msg=Hello".equals(value)) {
                         out.write("Hello".getBytes());
                     }
-                    if (!value.isEmpty() && !value.equals("msg=Hello")) {
+                    if (!value.isEmpty() && !"msg=Hello".equals(value)) {
                         out.write(value.split("=")[1].getBytes());
                     }
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
